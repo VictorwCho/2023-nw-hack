04ab3e6d778f67631d3e7d58d4655245dd993860
Cheryl Lau <clau136@my.bcit.ca>
2023-01-21 21:53:34 -0800

    Implemented Firebase Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,46 +7,57 @@ import { AppComponent } from './app.component';
 import {FirebaseUIModule, firebase, firebaseui} from 'firebaseui-angular';
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
-import { environment } from 'src/environment';
+import { environment } from 'src/environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
-const firebaseUiAuthConfig: firebaseui.auth.Config = {
-  signInFlow: 'popup',
-  signInOptions: [
-      {
-          scopes: [
-              'public_profile',
-              'email',
-              'user_likes',
-              'user_friends'
-          ],
-          customParameters: {
-              'auth_type': 'reauthenticate'
-          },
-          provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
-      },
-      {
-          requireDisplayName: false,
-          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
-      },
-  ],
-  //term of service
-  tosUrl: '<your-tos-link>',
-  //privacy url
-  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-  //credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE
-};
+// const firebaseUiAuthConfig: firebaseui.auth.Config = {
+//   signInFlow: 'popup',
+//   signInOptions: [
+//       {
+//           scopes: [
+//               'public_profile',
+//               'email',
+//               'user_likes',
+//               'user_friends'
+//           ],
+//           customParameters: {
+//               'auth_type': 'reauthenticate'
+//           },
+//           provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+//       },
+//       {
+//           requireDisplayName: false,
+//           provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+//       },
+//   ],
+//   //term of service
+//   tosUrl: '<your-tos-link>',
+//   //privacy url
+//   privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+//   //credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+//   credentialHelper: firebaseui.auth.CredentialHelper.NONE
+// };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
+    // AngularFireAuthModule,
+    // FirebaseUIModule.forRoot(firebaseUiAuthConfig),
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent]
