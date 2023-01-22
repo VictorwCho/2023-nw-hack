74725cbb09b4e4034b06ae0d1e68e73712ecdc91
lcchow <lcchow92@gmail.com>
2023-01-22 00:27:27 -0800

    changes to backend restul api

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const mongoString = process.env.DATABASE_URL
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use('/api', routes);
+app.use(cors());
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -16,12 +22,6 @@ database.on('error', (error) => {
 database.once('connected', () => {
     console.log('Database Connected');
 })
-
-const app = express();
-app.use(express.json());
-app.use('/api', routes)
-
-
 
 app.listen(3000, () => {
     console.log('Server Listening on port 3000')
